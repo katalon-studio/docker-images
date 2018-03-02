52068ac738faeac49ccb78ac25a13b6a53861dd1
Ubuntu <ubuntu@ip-192-168-1-221.ec2.internal>
2018-03-02 07:04:39 +0000

    upgrade Katalon Studio to 5.3.1

--- katalon/src/setup/katalon.sh
+++ katalon/src/setup/katalon.sh
@@ -4,9 +4,9 @@ set -xe
 
 echo "Install Katalon"
 
-version=5.3.0
+version=5.3.1
 package=Katalon_Studio_Linux_64-$version.tar.gz
-unzipped_directory=./Katalon_Studio_Linux_64-5.3
+unzipped_directory=./Katalon_Studio_Linux_64-$version
 
 wget -O $package http://download.katalon.com/$version/$package
 tar -xvzf $package
@@ -14,5 +14,6 @@ rm $package
 
 mv $unzipped_directory $KATALON_KATALON_INSTALL_DIR
 chmod u+x $KATALON_KATALON_INSTALL_DIR/katalon
+chmod u+x $KATALON_KATALON_INSTALL_DIR/configuration/resources/drivers/chromedriver_linux64/chromedriver
 
 echo "Katalon Studio $version" >> $KATALON_VERSION_FILE
