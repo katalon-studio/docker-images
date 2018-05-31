a9af35a772747a459242d93173bf201a223174d0
devalex88 <dev.alex.88@gmail.com>
2018-05-31 15:32:15 +0700

    update katalon version to 5.4.2

--- katalon/src/setup/katalon.sh
+++ katalon/src/setup/katalon.sh
@@ -4,15 +4,17 @@ set -xe
 
 echo "Install Katalon"
 
-version=5.4.2
-name=Katalon_Studio_Linux_64-$version
-package=$name.tar.gz
+version=5.4.1
+package=Katalon_Studio_Linux_64-$version.tar.gz
+unzipped_directory=./Katalon_Studio_Linux-$version
 
 wget -O $package http://download.katalon.com/$version/$package
-tar -xvzf $package -C $KATALON_KATALON_INSTALL_DIR_PARENT
+mkdir $unzipped_directory
+tar -xvzf $package -C $unzipped_directory
+ls
 rm $package
 
-mv $KATALON_KATALON_INSTALL_DIR_PARENT/$name KATALON_KATALON_INSTALL_DIR
+mv $unzipped_directory $KATALON_KATALON_INSTALL_DIR
 chmod u+x $KATALON_KATALON_INSTALL_DIR/katalon
 chmod u+x $KATALON_KATALON_INSTALL_DIR/configuration/resources/drivers/chromedriver_linux64/chromedriver
 
