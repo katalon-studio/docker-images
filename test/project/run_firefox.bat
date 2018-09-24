90e1553d21de961a625091550eeee988e682a32b
Alex Smith <dev.alex.88@gmail.com>
2018-09-24 14:29:45 +0700

    update

--- test/project/run_firefox.bat
+++ test/project/run_firefox.bat
@@ -10,6 +10,6 @@ rmdir /s /q "%reports_dir%"
 mkdir "%reports_dir%"
 
 set katalon_opts="-browserType=\"Firefox\" -retry=0 -statusDelay=15 -testSuitePath=\"Test Suites/TS_RegressionTest\""
-docker run --rm -v "%project_dir%":/katalon/katalon/source:ro -v "%reports_dir%":/katalon/katalon/report -e KATALON_OPTS=%katalon_opts% katalonstudio/katalon:test
+docker run --rm -v "%project_dir%":/katalon/katalon/source:ro -v "%reports_dir%":/katalon/katalon/report -e KATALON_OPTS=%katalon_opts% katalon-katalon
 
 cd "%current_dir%"
