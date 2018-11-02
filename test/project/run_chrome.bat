4c52706ced0dce1bc5746d42152d41c7e3957126
Alex Smith <dev.alex.88@gmail.com>
2018-11-02 12:00:40 +0700

    update build script

--- test/project/run_chrome.bat
+++ test/project/run_chrome.bat
@@ -2,4 +2,4 @@
 
 set katalon_opts="-browserType=\"Chrome\" -retry=0 -statusDelay=15 -testSuitePath=\"Test Suites/TS_RegressionTest\""
 
-docker run --rm -e KATALON_OPTS=%katalon_opts% -v .:/katalon/katalon/source katalon-katalon katalon-execute.sh
+docker run --rm -e KATALON_OPTS=%katalon_opts% -v "%cd%":/tmp/source -w /tmp/source katalon-katalon katalon-execute.sh
