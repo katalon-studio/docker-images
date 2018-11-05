a046749a1ab1421548a8d0a8e45d44e03f2b60fb
Alex Smith <dev.alex.88@gmail.com>
2018-11-05 16:15:14 +0700

    update build script

--- katalon/src/scripts/katalon-execute.sh
+++ katalon/src/scripts/katalon-execute.sh
@@ -41,6 +41,7 @@ project_file=$(find $project_dir -maxdepth 5 -type f -name "*.prj" -print -quit)
 
 cmd="$KATALON_KATALON_INSTALL_DIR/katalon -runMode=console -reportFolder=$report_dir -projectPath=$project_file $KATALON_OPTS"
 
+export DISPLAY=:99
 cd $workspace_dir
 eval "$cmd"
 
