2702897cf4f9b3820dbabf04812d785f5ea5b827
Alex Smith <dev.alex.88@gmail.com>
2018-11-06 09:31:03 +0700

    update build script

--- base/src/scripts/xvfb.sh
+++ base/src/scripts/xvfb.sh
@@ -5,5 +5,5 @@ set -xe
 echo "Starting X"
 
 sudo -E /usr/bin/Xvfb $DISPLAY $DISPLAY_CONFIGURATION &
-sudo -E while [ ! -e /tmp/.X11-unix/X99 ]; do sleep 0.1; done
+while [ ! -e /tmp/.X11-unix/X99 ]; do sleep 0.1; done
 sudo -E xhost +
