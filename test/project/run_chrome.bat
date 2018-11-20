0755c544eeed23551296a075339949a5964c3192
Alex <36872529+devalex88@users.noreply.github.com>
2018-11-20 15:40:27 +0700

    Update run_chrome.bat

--- test/project/run_chrome.bat
+++ test/project/run_chrome.bat
@@ -1,3 +1,9 @@
+-- Tested against Docker Toolbox.
+-- Make sure the corresponding Windows drives/directories has been added to VirtualBox's shared directories https://docs.docker.com/toolbox/toolbox_install_windows/#optional-add-shared-directories
+
 @echo on
 
-docker run -t --rm -v "%cd%:/tmp/source" -w /tmp/source katalonstudio/katalon:test katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"
+set currentdir=%cd::\=/%
+set currentdir=%currentdir:\=/%
+
+docker run -t --rm -v "/%currentdir%:/tmp/source" -w /tmp/source katalonstudio/katalon:test katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"
