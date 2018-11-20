ed0fbac765a43176ccf81b86275dabff09bd410f
Alex Smith <dev.alex.88@gmail.com>
2018-11-20 15:34:54 +0700

    remove windows samples

--- test/project/run_firefox.bat
+++
@@ -1,3 +0,0 @@
-@echo on
-
-docker run -t --rm -v "%cd%:/tmp/source" -w /tmp/source katalonstudio/katalon:test katalon-execute.sh -browserType="Firefox" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"
