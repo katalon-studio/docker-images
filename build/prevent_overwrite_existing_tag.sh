0e9d477f7f20df2b94da85f323668ca4f73304b3
Alex Smith <dev.alex.88@gmail.com>
2019-05-09 11:34:34 +0700

    update scripts

--- build/prevent_overwrite_existing_tag.sh
+++ build/prevent_overwrite_existing_tag.sh
@@ -2,12 +2,8 @@
 
 set -xe
 
-ksversion ="$1"
+ksversion =$1
 
-function docker_tag_exists() {
-  curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
-}
-
-if docker_tag_exists katalonstudio/katalon $ksversion; then
+if curl --silent -f -lSL https://index.docker.io/v1/repositories/katalonstudio/katalon/tags/$ksversion > /dev/null; then
   exit 1
 fi
