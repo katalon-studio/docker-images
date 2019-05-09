4fceaa616cd1aacc1508e67c1fea642eb931dd80
Alex Smith <dev.alex.88@gmail.com>
2019-05-09 11:40:05 +0700

    update scripts

--- build/prevent_overwrite_existing_tag.sh
+++ build/prevent_overwrite_existing_tag.sh
@@ -4,6 +4,4 @@ set -xe
 
 ksversion=$1
 
-if curl --silent -f -lSL https://index.docker.io/v1/repositories/katalonstudio/katalon/tags/$ksversion > /dev/null; then
-  exit 1
-fi
+curl --silent -f -lSL https://index.docker.io/v1/repositories/katalonstudio/katalon/tags/$ksversion > /dev/null
