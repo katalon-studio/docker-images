811d77e22f0169bbbc711a8690efed7e7989321b
Alex <36872529+devalex88@users.noreply.github.com>
2019-05-14 14:50:03 +0700

    Update prevent_overwrite_existing_tag.sh

--- build/prevent_overwrite_existing_tag.sh
+++ build/prevent_overwrite_existing_tag.sh
@@ -5,7 +5,7 @@ set -xe
 ksversion=$1
 
 if curl --output /dev/null --silent --head --fail https://hub.docker.com/v2/repositories/katalonstudio/katalon/tags/$ksversion; then
-  exit 1
-else
   exit 0
+else
+  exit 1
 fi
