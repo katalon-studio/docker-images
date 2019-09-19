3233a30077b880b2d3e7045bdd4a4d51694896f8
Qui Le <quile@kms-technology.com>
2019-09-19 13:59:31 +0700

    update

--- katalon/src/setup/katalon.sh
+++ katalon/src/setup/katalon.sh
@@ -18,6 +18,18 @@ rm $package
 mv $KATALON_KATALON_INSTALL_DIR_PARENT/$unzipped_directory $KATALON_KATALON_INSTALL_DIR
 chmod u+x $KATALON_KATALON_INSTALL_DIR/katalon
 
-chmod -R u+x /opt/katalonstudio/configuration/resources/drivers
+chmod -R u+x $KATALON_KATALON_INSTALL_DIR/configuration/resources/drivers
+
+rm $KATALON_KATALON_INSTALL_DIR/configuration/resources/drivers/chromedriver_linux64/chromedriver
+wget -O chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/78.0.3904.11/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d $KATALON_KATALON_INSTALL_DIR/configuration/resources/drivers/chromedriver_linux64
+rm chromedriver_linux64.zip
+
+rm $KATALON_KATALON_INSTALL_DIR/configuration/resources/drivers/firefox_linux64/geckodriver
+wget -O geckodriver-linux64.tar.gz https://github.com/mozilla/geckodriver/releases/download/v0.25.0/geckodriver-v0.25.0-linux64.tar.gz
+tar -xvzf geckodriver-linux64.tar.gz -C $KATALON_KATALON_INSTALL_DIR/configuration/resources/drivers/firefox_linux64
+rm geckodriver-linux64.tar.gz
+
+chmod -R u+x $KATALON_KATALON_INSTALL_DIR/configuration/resources/drivers
 
 echo "Katalon Studio $version" >> $KATALON_VERSION_FILE
