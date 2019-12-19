a47711ae841958319be232d07f714d267cf6cc94
Alex <dev.alex.88@gmail.com>
2019-12-19 23:50:47 +0700

    update

---
+++ src/scripts/katalon-notify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -xe
+
+cat $KATALON_VERSION_FILE
+
+echo "Thank you for using Katalon Studio. Please visit https://github.com/katalon-studio/docker-images for usage."
--- test/project/run_chrome.sh
+++ test/project/run_chrome.sh
@@ -5,4 +5,6 @@ set -xe
 ksversion=$1
 apiKey=$2
 
-docker run -t --rm  -u $(id -u ${USER}):$(id -g ${USER}) -v "/$(pwd)":/katalon/katalon/source katalonstudio/katalon:$ksversion katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest" -apiKey=${apiKey}
+docker run -t --rm  -e LOCAL_USER_ID=`id -u $USER` -v "/$(pwd)":/katalon/katalon/source katalon katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest" -apiKey=${apiKey}
+
+# docker run -t --rm  -e LOCAL_USER_ID=`id -u $USER` -v "/$(pwd)":/katalon/katalon/source katalonstudio/katalon:$ksversion katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest" -apiKey=${apiKey}
