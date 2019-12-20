33595f0560c4ac8669f95cacb6b1ab03b65c1258
Alex <dev.alex.88@gmail.com>
2019-12-20 16:13:01 +0700

    update

--- build/build.sh
+++ build/build.sh
@@ -4,5 +4,4 @@ set -xe
 
 ksversion=$1
 
-docker build -t katalon-base -f base/Dockerfile base
-docker build -t katalon-katalon -f katalon/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion katalon
+docker build -t katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
