fac95f18f77754fde0e5dea77af34e51c8c45c5d
Alex <dev.alex.88@gmail.com>
2019-12-20 23:04:37 +0700

    update

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -8,7 +8,9 @@ if [ -z "$KATALON_USER_ID" ]; then
     exec "$@"
 else
     echo "Starting with UID : $KATALON_USER_ID"
-    useradd --shell /bin/bash -u $KATALON_USER_ID -o -c "" -m katalon
-    export HOME=/home/katalon
-    exec gosu katalon "$@"
+    username=katalon
+    useradd --shell /bin/bash -u $KATALON_USER_ID -o -c "" -m $username
+    export HOME=/home/$username
+    chown -R $KATALON_USER_ID $KATALON_SOFTWARE_DIR
+    exec gosu $username "$@"
 fi
