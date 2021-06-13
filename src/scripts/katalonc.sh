dfe5684cff27f51fe511340a952e4d6ba855780f
Vadym Mukha <vadifly@gmail.com>
2021-06-13 21:01:57 +0200

    When I wanna use self-sign certificates

--- src/scripts/katalonc.sh
+++ src/scripts/katalonc.sh
@@ -7,6 +7,11 @@ echo "Starting Katalon Studio"
 
 cat $KATALON_VERSION_FILE
 
+# When I wanna use self-sign certificates
+# in docker -v ./certs:/usr/local/share/ca-certificates
+echo "Update CA certificates"
+/usr/sbin/update-ca-certificates
+
 args=("$KATALON_KATALON_INSTALL_DIR/katalonc" "$@")
 
 xvfb-run -s "-screen 0 $DISPLAY_CONFIGURATION" "${args[@]}"
