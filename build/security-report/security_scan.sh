e83c1ca3fdf3b7a623847c83ce6c3442ce2a704e
Huyen Nguyen <huyen.nguyen@katalon.com>
2022-07-26 22:06:43 +0700

    update

--- build/security-report/security_scan.sh
+++ build/security-report/security_scan.sh
@@ -10,11 +10,11 @@ npm install snyk-to-html --location=global
 sudo apt-get update -qq
 sudo apt-get install -y zip
 
-echo "Scanning security severity with Trivy..."
+echo "Scanning security severity ${katalonImage} with Trivy..."
 docker pull aquasec/trivy:latest
 docker run -v $(pwd):/security-report --rm aquasec/trivy:0.18.3 i --format template --template "@contrib/html.tpl" --output /security-report/security_report_trivy.html ${katalonImage}
 
-echo "Scanning security severity with Snyk..."
+echo "Scanning security severity ${katalonImage} with Snyk..."
 yes 2>/dev/null | docker scan --login --token "8a4cc0b6-50db-40e3-8afd-f8ce97e1b12c"
 echo $(yes | docker scan ${katalonImage} --json) >> security_report_snyk.json
 snyk-to-html -i security_report_snyk.json -o security_report_snyk.html
--- build/security-report/upload.sh
+++ build/security-report/upload.sh
@@ -8,7 +8,7 @@ pip3 install boto3
 
 # Run upload script
 echo "Uploading Trivy report..."
-python3 build/report/upload_file_to_s3.py $bucket_name "${ks_version}/security_report_trivy.html" $aws_access_key $aws_access_secret "security_report_trivy.html"
+python3 build/security-report/upload_file_to_s3.py $bucket_name "${ks_version}/security_report_trivy.html" $aws_access_key $aws_access_secret "security_report_trivy.html"
 
 echo "Uploading Snyk report..."
-python3 build/report/upload_file_to_s3.py $bucket_name "${ks_version}/security_report_snyk.html" $aws_access_key $aws_access_secret "security_report_snyk.html"
+python3 build/security-report/upload_file_to_s3.py $bucket_name "${ks_version}/security_report_snyk.html" $aws_access_key $aws_access_secret "security_report_snyk.html"
