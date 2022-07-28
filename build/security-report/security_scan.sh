1ddad1682014197ec0e20feb80c0cbf4511ae0a9
Huyen Nguyen <huyen.nguyen@katalon.com>
2022-07-28 15:06:01 +0700

    apply snyk tokeen

--- build/security-report/security_scan.sh
+++ build/security-report/security_scan.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ks_version_tag=$1
+snyk_token=$2
 set -xe
 
 katalonImage=katalonstudio/katalon:$ks_version_tag
@@ -15,6 +16,6 @@ docker pull aquasec/trivy:latest
 docker run -v $(pwd):/security-report --rm aquasec/trivy:0.18.3 i --format template --template "@contrib/html.tpl" --output /security-report/security_report_trivy.html ${katalonImage}
 
 echo "Scanning security severity ${katalonImage} with Snyk..."
-yes 2>/dev/null | docker scan --login --token "8a4cc0b6-50db-40e3-8afd-f8ce97e1b12c"
+yes 2>/dev/null | docker scan --login --token $snyk_token
 echo $(yes | docker scan ${katalonImage} --json) >> security_report_snyk.json
 snyk-to-html -i security_report_snyk.json -o security_report_snyk.html
