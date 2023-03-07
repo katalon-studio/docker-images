4aa19e713fe08722f700ec34fd0d0182ec4d3560
Minh-Chien Trang <93188636+Minh-Chien@users.noreply.github.com>
2023-03-07 14:32:27 +0700

    Build docker image to test auto update browser

--- src/scripts/katalonc.sh
+++ src/scripts/katalonc.sh
@@ -7,6 +7,15 @@ echo "Starting Katalon Studio"
 
 cat $KATALON_VERSION_FILE
 
+cat "Upgrade Chrome browser..."
+apt --only-upgrade install google-chrome-stable 
+
+cat "Upgrade Firefox browser..."
+apt --only-upgrade install firefox
+
+cat "Upgrade Edge Chromium browser..."
+apt --only-upgrade install microsoft-edge-stable
+
 args=("$KATALON_KATALON_INSTALL_DIR/katalonc" "$@")
 
 xvfb-run -s "-screen 0 $DISPLAY_CONFIGURATION" "${args[@]}"
