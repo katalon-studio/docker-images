23b05aec5337e4cee98051eafaeae64f02da5613
Minh-Chien Trang <93188636+Minh-Chien@users.noreply.github.com>
2023-03-08 10:10:56 +0700

    Test checking browser type to upgrade

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,15 +6,17 @@ echo "Entrypoint"
 
 if [ -z "$KATALON_USER_ID" ]; then
 	if [ "$1" == "-docker.autoUpdateBrowsers=true" ]; then
-		echo "Upgrade Chrome browser..."
-		apt --only-upgrade install google-chrome-stable 
-
-		# echo "Upgrade Firefox browser..."
-		# apt --only-upgrade install firefox
-
-		# echo "Upgrade Edge Chromium browser..."
-		# apt --only-upgrade install microsoft-edge-stable
-		echo "${@:2}"
+		if grep -q "-browserType=\"Chrome\"" <<< "$@"; then
+			echo "Upgrade Chrome browser..."
+			apt --only-upgrade install google-chrome-stable
+		elif grep -q "-browserType=\"Firefox\"" <<< "$@"; then
+			echo "Upgrade Firefox browser..."
+			apt --only-upgrade install firefox
+		elif grep -q "-browserType=\"Edge Chromium\"" <<< "$@"; then
+			echo "Upgrade Edge Chromium browser..."
+			apt --only-upgrade install microsoft-edge-stable
+		fi
+		
 		exec "${@:2}"
 	else	
 		exec "$@"
