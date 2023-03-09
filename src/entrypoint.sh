ab69d0ccf380b9c4a7ca560dac86e05e6da59eb4
Minh-Chien Trang <93188636+Minh-Chien@users.noreply.github.com>
2023-03-09 15:54:01 +0700

    Not use the upgrade-web-browsers.sh file

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,7 +4,19 @@ set -xe
 
 echo "Entrypoint"
 
-exec upgrade-web-browsers.sh "$@"
+# exec upgrade-web-browsers.sh "$@"
+if grep -q -- "-docker.autoUpdateBrowsers=true" <<< "$@"; then
+	if grep -q -- "-browserType=Chrome" <<< "$@"; then
+		echo "Upgrade Chrome browser..."
+		apt --only-upgrade install google-chrome-stable
+	elif grep -q -- "-browserType=Firefox" <<< "$@"; then
+		echo "Upgrade Firefox browser..."
+		apt --only-upgrade install firefox
+	elif grep -q -- "-browserType=Edge Chromium" <<< "$@"; then
+		echo "Upgrade Edge Chromium browser..."
+		apt --only-upgrade install microsoft-edge-stable
+	fi
+fi
 
 if [ -z "$KATALON_USER_ID" ]; then
 	exec "$@"
