df605fa7f7a4e6ec0e34d9fac1857a426872367f
Minh-Chien Trang <93188636+Minh-Chien@users.noreply.github.com>
2023-03-09 14:33:04 +0700

    Grant users id access to system resources

--- src/scripts/katalonc.sh
+++ src/scripts/katalonc.sh
@@ -10,13 +10,13 @@ cat $KATALON_VERSION_FILE
 if grep -q -- "-docker.autoUpdateBrowsers=true" <<< "$@"; then
 	if grep -q -- "-browserType=Chrome" <<< "$@"; then
 		echo "Upgrade Chrome browser..."
-		apt --only-upgrade install google-chrome-stable
+		sudo apt --only-upgrade install google-chrome-stable
 	elif grep -q -- "-browserType=Firefox" <<< "$@"; then
 		echo "Upgrade Firefox browser..."
-		apt --only-upgrade install firefox
+		sudo apt --only-upgrade install firefox
 	elif grep -q -- "-browserType=Edge Chromium" <<< "$@"; then
 		echo "Upgrade Edge Chromium browser..."
-		apt --only-upgrade install microsoft-edge-stable
+		sudo apt --only-upgrade install microsoft-edge-stable
 	fi
 fi
 
