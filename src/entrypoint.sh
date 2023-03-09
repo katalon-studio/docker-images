aa9d3905a273407e93eb8a5506cbe4cbf0a4d676
Minh-Chien Trang <93188636+Minh-Chien@users.noreply.github.com>
2023-03-09 09:18:20 +0700

    Add two hyphens before a string containing a hyphen in grep expression

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,13 +6,13 @@ echo "Entrypoint"
 
 if [ -z "$KATALON_USER_ID" ]; then
 	if [ "$1" == "-docker.autoUpdateBrowsers=true" ]; then
-		if grep -q "-browserType=Chrome" <<< "$@"; then
+		if grep -q -- "-browserType=Chrome" <<< "$@"; then
 			echo "Upgrade Chrome browser..."
 			apt --only-upgrade install google-chrome-stable
-		elif grep -q "-browserType=Firefox" <<< "$@"; then
+		elif grep -q -- "-browserType=Firefox" <<< "$@"; then
 			echo "Upgrade Firefox browser..."
 			apt --only-upgrade install firefox
-		elif grep -q "-browserType=Edge Chromium" <<< "$@"; then
+		elif grep -q -- "-browserType=Edge Chromium" <<< "$@"; then
 			echo "Upgrade Edge Chromium browser..."
 			apt --only-upgrade install microsoft-edge-stable
 		fi
