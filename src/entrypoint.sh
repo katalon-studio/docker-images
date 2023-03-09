ff14516657cdc6ef865918a6955b2ddeba063a5a
Minh-Chien Trang <93188636+Minh-Chien@users.noreply.github.com>
2023-03-09 13:54:48 +0700

    Check the new argument in katalonc.sh file

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,22 +5,7 @@ set -xe
 echo "Entrypoint"
 
 if [ -z "$KATALON_USER_ID" ]; then
-	if [ "$1" == "-docker.autoUpdateBrowsers=true" ]; then
-		if grep -q -- "-browserType=Chrome" <<< "$@"; then
-			echo "Upgrade Chrome browser..."
-			apt --only-upgrade install google-chrome-stable
-		elif grep -q -- "-browserType=Firefox" <<< "$@"; then
-			echo "Upgrade Firefox browser..."
-			apt --only-upgrade install firefox
-		elif grep -q -- "-browserType=Edge Chromium" <<< "$@"; then
-			echo "Upgrade Edge Chromium browser..."
-			apt --only-upgrade install microsoft-edge-stable
-		fi
-		
-		exec "${@:2}"
-	else	
-		exec "$@"
-	fi
+	exec "$@"
 else
     echo "Starting with UID : $KATALON_USER_ID"
     username=katalon
--- src/scripts/katalonc.sh
+++ src/scripts/katalonc.sh
@@ -7,6 +7,19 @@ echo "Starting Katalon Studio"
 
 cat $KATALON_VERSION_FILE
 
+if grep -q -- "-docker.autoUpdateBrowsers=true" <<< "$@"; then
+	if grep -q -- "-browserType=Chrome" <<< "$@"; then
+		echo "Upgrade Chrome browser..."
+		apt --only-upgrade install google-chrome-stable
+	elif grep -q -- "-browserType=Firefox" <<< "$@"; then
+		echo "Upgrade Firefox browser..."
+		apt --only-upgrade install firefox
+	elif grep -q -- "-browserType=Edge Chromium" <<< "$@"; then
+		echo "Upgrade Edge Chromium browser..."
+		apt --only-upgrade install microsoft-edge-stable
+	fi
+fi
+
 args=("$KATALON_KATALON_INSTALL_DIR/katalonc" "$@")
 
 xvfb-run -s "-screen 0 $DISPLAY_CONFIGURATION" "${args[@]}"
