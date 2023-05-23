7c66d2a7bc53dd142dea8e2d5ca1d9f4d08834da
linh to <linh.to@katalon.com>
2023-05-23 16:36:24 +0700

    retry

--- build/build.sh
+++ build/build.sh
@@ -4,4 +4,4 @@ set -xe
 
 ksversion=$1
 docker buildx create --use
-docker buildx build  -t katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion --platform=linux/amd64,linux/arm64.
+docker buildx build --platform=linux/amd64,linux/arm64 -t katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion --push .
