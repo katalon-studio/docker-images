3f10445789908b331ace0bc8e57ce48604c8075b
linh to <linh.to@katalon.com>
2023-05-23 12:04:25 +0700

    try to build platforms

--- build/build.sh
+++ build/build.sh
@@ -3,5 +3,5 @@
 set -xe
 
 ksversion=$1
-
+docker buildx create --use
 docker buildx build --platform=linux/amd64,linux/arm64 -t katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
