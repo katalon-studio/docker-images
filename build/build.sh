753dfba3a5527f0720c2499ddf05543f8b7c7c54
linh to <linh.to@katalon.com>
2023-05-23 16:03:09 +0700

    Revert "print log"
    This reverts commit 69adfe040a187ed476b9dd0c8a6a2d97fd62a6b2.

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 set -xe
-echo "Build"
+
 ksversion=$1
 docker buildx create --use
-docker buildx build --platform=linux/amd64,linux/arm64 -t katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
-echo "Built done"
+docker buildx build  -t katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion --platform=linux/amd64,linux/arm64.
--- src/cleanup.sh
+++ src/cleanup.sh
@@ -7,5 +7,3 @@ echo "Clean up"
 apt clean all
 rm -rf /var/lib/apt/lists/*
 cat $KATALON_VERSION_FILE
-
-echo "Clean up done"
