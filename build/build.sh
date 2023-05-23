69adfe040a187ed476b9dd0c8a6a2d97fd62a6b2
linh to <linh.to@katalon.com>
2023-05-23 12:57:43 +0700

    print log

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -xe
-
+echo "Build"
 ksversion=$1
 docker buildx create --use
 docker buildx build --platform=linux/amd64,linux/arm64 -t katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
+echo "Built done"
--- src/cleanup.sh
+++ src/cleanup.sh
@@ -7,3 +7,5 @@ echo "Clean up"
 apt clean all
 rm -rf /var/lib/apt/lists/*
 cat $KATALON_VERSION_FILE
+
+echo "Clean up done"
