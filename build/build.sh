f1d451ec51ed461b0edd78317871f5185c3e2d00
linhtnlK <linh.to@katalon.com>
2023-05-24 10:27:03 +0700

    build

--- build/build.sh
+++ build/build.sh
@@ -3,4 +3,5 @@
 set -xe
 
 ksversion=$1
-docker build -t katalon-katalon -f src/Dockerfile --build-arg ARCH=arm64v8 KATALON_STUDIO_VERSION=$ksversion .
+docker buildx ls
+docker buildx build --platform linux/amd64,linux/arm64 -t katalon-katalon -f src/Dockerfile --build-arg  KATALON_STUDIO_VERSION=$ksversion .
