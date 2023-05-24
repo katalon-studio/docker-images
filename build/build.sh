c281e1c07ded9418187535e2fa15adb63983cc62
linh to <linh.to@katalon.com>
2023-05-24 09:56:39 +0700

    rebuild

--- build/build.sh
+++ build/build.sh
@@ -3,4 +3,4 @@
 set -xe
 
 ksversion=$1
-docker build -t katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion ARCH=arm64v8.
+docker build -t katalon-katalon -f src/Dockerfile --build-arg ARCH=arm64v8 KATALON_STUDIO_VERSION=$ksversion .
