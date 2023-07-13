02fc9cc41a070b6784ab394079cecc89319b1466
HieuBui <hieu.bui@katalon.com>
2023-07-13 13:44:00 +0700

    update

--- build/security-report/security_scan.sh
+++ build/security-report/security_scan.sh
@@ -11,12 +11,12 @@ npm install snyk-to-html --location=global
 sudo apt-get update -qq
 sudo apt-get install -y zip
 
-echo "Scanning security severity ${katalonImage} with Trivy..."
-docker pull aquasec/trivy:latest
-docker run -v $(pwd):/security-report --rm aquasec/trivy:0.18.3 i --format template --template "@contrib/html.tpl" --output /security-report/security_report_trivy.html ${katalonImage} 
-
 sudo apt install docker-scan-plugin
 echo "Scanning security severity ${katalonImage} with Snyk..."
 yes 2>/dev/null | docker scan --login --token $snyk_token
 echo $(yes | docker scan ${katalonImage} --json) >> security_report_snyk.json
 snyk-to-html -i security_report_snyk.json -o security_report_snyk.html
+
+echo "Scanning security severity ${katalonImage} with Trivy..."
+docker pull aquasec/trivy:latest
+docker run -v $(pwd):/security-report --rm aquasec/trivy:0.18.3 i --format template --template "@contrib/html.tpl" --output /security-report/security_report_trivy.html ${katalonImage} 
