e7c691fc81b4fc62efc89adcb68dc51c13130769
HieuBui <hieu.bui@katalon.com>
2023-08-21 09:52:59 +0700

    test upload 9

--- build/copy.sh
+++ build/copy.sh
@@ -4,5 +4,5 @@ set -xe
 
 ksversion=$1
 
-docker cp katalonstudio/katalon:$ksversion:/security-report/security_report_trivy.html ~/security_report_trivy.html
-docker cp katalonstudio/katalon:$ksversion:/security_report_snyk.html ~/security_report_snyk.html
+docker cp katalon:/security-report/security_report_trivy.html ~/security_report_trivy.html
+docker cp katalon:/security_report_snyk.html ~/security_report_snyk.html
