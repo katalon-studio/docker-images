8cbd7ad2d8b563a37fd0ba346382b908e3042e21
HieuBui <hieu.bui@katalon.com>
2023-11-01 14:40:09 +0700

    build base image only

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -79,30 +79,30 @@ ls $GRADLE_HOME
 mkdir -p $KATALON_KATALON_ROOT_DIR
 cd $KATALON_KATALON_ROOT_DIR
 
-echo "Install Katalon"
-katalon_version="$KATALON_STUDIO_VERSION"
-katalon_directory="$version"
-katalon_package="Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz"
-katalon_unzipped_directory="Katalon_Studio_Engine_Linux_64-$katalon_version"
-# general link
-wget -O $katalon_package https://download.katalon.com/$katalon_version/Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz
-# beta link
-# wget -O $katalon_package https://download.katalon.com/release-beta/$katalon_version/Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz
-ls
-tar -xvzf $katalon_package -C $KATALON_KATALON_INSTALL_DIR_PARENT
-ls
-rm $katalon_package
-mv $KATALON_KATALON_INSTALL_DIR_PARENT/$katalon_unzipped_directory $KATALON_KATALON_INSTALL_DIR
-chmod u+x $KATALON_KATALON_INSTALL_DIR/katalonc
-chmod -R u+x $KATALON_KATALON_INSTALL_DIR/configuration/resources/drivers
-echo "Katalon Studio $version" >> $KATALON_VERSION_FILE
+# echo "Install Katalon"
+# katalon_version="$KATALON_STUDIO_VERSION"
+# katalon_directory="$version"
+# katalon_package="Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz"
+# katalon_unzipped_directory="Katalon_Studio_Engine_Linux_64-$katalon_version"
+# # general link
+# wget -O $katalon_package https://download.katalon.com/$katalon_version/Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz
+# # beta link
+# # wget -O $katalon_package https://download.katalon.com/release-beta/$katalon_version/Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz
+# ls
+# tar -xvzf $katalon_package -C $KATALON_KATALON_INSTALL_DIR_PARENT
+# ls
+# rm $katalon_package
+# mv $KATALON_KATALON_INSTALL_DIR_PARENT/$katalon_unzipped_directory $KATALON_KATALON_INSTALL_DIR
+# chmod u+x $KATALON_KATALON_INSTALL_DIR/katalonc
+# chmod -R u+x $KATALON_KATALON_INSTALL_DIR/configuration/resources/drivers
+# echo "Katalon Studio $version" >> $KATALON_VERSION_FILE
 
-chmod -R 777 $KATALON_ROOT_DIR
-chmod -R 777 $KATALON_SOFTWARE_DIR
+# chmod -R 777 $KATALON_ROOT_DIR
+# chmod -R 777 $KATALON_SOFTWARE_DIR
 
 # clean up
 
 echo "Clean up"
 apt clean all
 rm -rf /var/lib/apt/lists/*
-cat $KATALON_VERSION_FILE
+# cat $KATALON_VERSION_FILE
