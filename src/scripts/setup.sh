143784a8ce475182c3ca6d379d13e80aff451ebc
HieuBui <hieu.bui@katalon.com>
2023-12-11 16:39:24 +0700

    change to -dev version

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -80,7 +80,7 @@ mkdir -p $KATALON_KATALON_ROOT_DIR
 cd $KATALON_KATALON_ROOT_DIR
 
 echo "Install Katalon"
-katalon_version="$KATALON_STUDIO_VERSION"
+katalon_version="$KATALON_STUDIO_VERSION-dev"
 katalon_directory="$version"
 katalon_package="Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz"
 katalon_unzipped_directory="Katalon_Studio_Engine_Linux_64-$katalon_version"
