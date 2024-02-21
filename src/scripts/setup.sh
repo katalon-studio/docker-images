2efaddf1e11c4fbd3750f970f162c87e9ad30eec
Coty Rosenblath <coty@katalon.com>
2024-02-21 00:06:12 -0500

    Add Mozilla Firefox repository and install Firefox via setup script
    Added commands to add Mozilla Firefox repository key, source, preferences, and updated the package list before installing Firefox in the setup script.

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -40,6 +40,15 @@ apt -y install fonts-tlwg-loma-otf
 apt -y install fonts-ubuntu
 
 echo "Install Mozilla Firefox"
+install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | tee /etc/apt/preferences.d/mozilla
+apt update
 apt -y install firefox
 # Install 'pulseaudio' package to support WebRTC audio streams
 apt -y install pulseaudio
