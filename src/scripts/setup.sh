ff110ecdbdb21d022c58e8ab297daa7d27916cd2
linhtnl <lamlinh20001@gmail.com>
2024-02-24 23:19:56 +0700

    build docker for cucumber v7

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -85,7 +85,8 @@ katalon_directory="$version"
 katalon_package="Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz"
 katalon_unzipped_directory="Katalon_Studio_Engine_Linux_64-$katalon_version"
 # general link
-wget -O $katalon_package https://download.katalon.com/$katalon_version/Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz
+wget -O $katalon_package https://download.katalon.com/9.3.1-snapshot/rnd-cucumber7/Katalon_Studio_Engine_Linux_64-9.3.1.tar.gz
+
 # beta link
 # wget -O $katalon_package https://download.katalon.com/release-beta/$katalon_version/Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz
 ls
