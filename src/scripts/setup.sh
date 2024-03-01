f0eabb4b065fd96a77bdc2149c2b127fba172446
an.qnguyen <>
2024-03-01 18:39:52 +0700

    test

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -2,82 +2,82 @@
 
 set -xe
 
-mkdir -p $KATALON_BASE_ROOT_DIR
-cd $KATALON_BASE_ROOT_DIR
+# mkdir -p $KATALON_BASE_ROOT_DIR
+# cd $KATALON_BASE_ROOT_DIR
 
-apt-get update
+# apt-get update
 
-echo "Install tools"
-apt -y install apt-utils
-apt -y install wget
-apt -y install unzip
-apt -y install curl
-apt -y install gosu
-apt -y install psmisc
+# echo "Install tools"
+# apt -y install apt-utils
+# apt -y install wget
+# apt -y install unzip
+# apt -y install curl
+# apt -y install gosu
+# apt -y install psmisc
 
-echo "Install JRE"
-apt -y install openjdk-17-jdk
+# echo "Install JRE"
+# apt -y install openjdk-17-jdk
 
-echo "Install CircleCI tools"
-apt -y install git
-apt -y install ssh
-apt -y install tar
-apt -y install gzip
-apt -y install ca-certificates
+# echo "Install CircleCI tools"
+# apt -y install git
+# apt -y install ssh
+# apt -y install tar
+# apt -y install gzip
+# apt -y install ca-certificates
 
-echo "Install Xvfb"
-apt -y install xvfb
+# echo "Install Xvfb"
+# apt -y install xvfb
 
-echo "Install fonts"
-apt -y install libfontconfig
-apt -y install libfreetype6
-apt -y install xfonts-cyrillic
-apt -y install xfonts-scalable
-apt -y install fonts-liberation
-apt -y install fonts-ipafont-gothic
-apt -y install fonts-wqy-zenhei
-apt -y install fonts-tlwg-loma-otf
-apt -y install ttf-ubuntu-font-family
+# echo "Install fonts"
+# apt -y install libfontconfig
+# apt -y install libfreetype6
+# apt -y install xfonts-cyrillic
+# apt -y install xfonts-scalable
+# apt -y install fonts-liberation
+# apt -y install fonts-ipafont-gothic
+# apt -y install fonts-wqy-zenhei
+# apt -y install fonts-tlwg-loma-otf
+# apt -y install ttf-ubuntu-font-family
 
-echo "Install Mozilla Firefox"
-apt -y install firefox
-# Install 'pulseaudio' package to support WebRTC audio streams
-apt -y install pulseaudio
-echo "$(firefox -version)" >> $KATALON_VERSION_FILE
+# echo "Install Mozilla Firefox"
+# apt -y install firefox
+# # Install 'pulseaudio' package to support WebRTC audio streams
+# apt -y install pulseaudio
+# echo "$(firefox -version)" >> $KATALON_VERSION_FILE
 
-echo "Install Google Chrome"
-chrome_package='google-chrome-stable_current_amd64.deb'
-wget -O $chrome_package  https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i $chrome_package || apt -y -f install
-rm $chrome_package
-echo "$(google-chrome --version)" >> $KATALON_VERSION_FILE || true
+# echo "Install Google Chrome"
+# chrome_package='google-chrome-stable_current_amd64.deb'
+# wget -O $chrome_package  https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# dpkg -i $chrome_package || apt -y -f install
+# rm $chrome_package
+# echo "$(google-chrome --version)" >> $KATALON_VERSION_FILE || true
 
-./wrap_chrome_binary.sh && rm -rfv ./wrap_chrome_binary.sh
+# ./wrap_chrome_binary.sh && rm -rfv ./wrap_chrome_binary.sh
 
-echo "Install Edge Chromium"
-microsoft_edge_package='MicrosoftEdgeSetup.exe'
-wget -O $microsoft_edge_package https://go.microsoft.com/fwlink?linkid=2149051
-dpkg -i $microsoft_edge_package || apt -y -f install
-rm $microsoft_edge_package
-echo "$(microsoft-edge --version)" >> $KATALON_VERSION_FILE || true
+# echo "Install Edge Chromium"
+# microsoft_edge_package='MicrosoftEdgeSetup.exe'
+# wget -O $microsoft_edge_package https://go.microsoft.com/fwlink?linkid=2149051
+# dpkg -i $microsoft_edge_package || apt -y -f install
+# rm $microsoft_edge_package
+# echo "$(microsoft-edge --version)" >> $KATALON_VERSION_FILE || true
 
-./wrap_edge_chromium_binary.sh && rm -rfv ./wrap_edge_chromium_binary.sh
+# ./wrap_edge_chromium_binary.sh && rm -rfv ./wrap_edge_chromium_binary.sh
 
-echo "Install Gradle"
-gradle_version='5.4.1'
-gradle_package="gradle-$gradle_version-bin.zip"
-gradle_unzipped_package="gradle-$gradle_version"
-wget https://downloads.gradle.org/distributions/gradle-$gradle_version-bin.zip
-ls
-unzip $gradle_package
-ls
-rm $gradle_package
-mv $gradle_unzipped_package $GRADLE_HOME
-ls $GRADLE_HOME
+# echo "Install Gradle"
+# gradle_version='5.4.1'
+# gradle_package="gradle-$gradle_version-bin.zip"
+# gradle_unzipped_package="gradle-$gradle_version"
+# wget https://downloads.gradle.org/distributions/gradle-$gradle_version-bin.zip
+# ls
+# unzip $gradle_package
+# ls
+# rm $gradle_package
+# mv $gradle_unzipped_package $GRADLE_HOME
+# ls $GRADLE_HOME
 
-# copy scripts
-mkdir -p $KATALON_KATALON_ROOT_DIR
-cd $KATALON_KATALON_ROOT_DIR
+# # copy scripts
+# mkdir -p $KATALON_KATALON_ROOT_DIR
+# cd $KATALON_KATALON_ROOT_DIR
 
 echo "Install Katalon"
 katalon_version="$KATALON_STUDIO_VERSION"
