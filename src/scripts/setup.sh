94c5d4682eef6f8f7a776725f34474e79e3f3a1e
linhtnlK <linh.to@katalon.com>
2024-03-26 15:55:39 +0700

    update link

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -85,7 +85,7 @@ katalon_directory="$version"
 katalon_package="Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz"
 katalon_unzipped_directory="Katalon_Studio_Engine_Linux_64-$katalon_version"
 # general link
-wget -O $katalon_package http://surl.li/rycna
+wget -O $katalon_package http://surl.li/rycww
 # beta link
 # wget -O $katalon_package https://download.katalon.com/release-beta/$katalon_version/Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz
 ls
