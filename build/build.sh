3b8c9a3b6330278b9dc4bc37584aa076e7c3a306
Huyen Nguyen <huyen.nguyen@katalon.com>
2024-09-30 17:07:06 +0700

    Config builder

--- build/build.sh
+++ build/build.sh
@@ -4,4 +4,6 @@ set -xe
 
 ksversion=$1
 
+docker buildx create --name mybuilder --use
+docker buildx inspect --bootstrap
 docker buildx build --platform linux/amd64,linux/arm64 -t katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
