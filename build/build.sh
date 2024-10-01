bea40aef5166071750787085fd339cdd844f5083
Huyen Nguyen <huyen.nguyen@katalon.com>
2024-10-02 06:59:57 +0700

    Enable Buildx

--- build/build.sh
+++ build/build.sh
@@ -3,7 +3,8 @@
 set -xe
 
 ksversion=$1
+export DOCKER_BUILDKIT=1
 
 docker buildx create --name mybuilder --use
 docker buildx inspect --bootstrap
-docker buildx build  --output type=docker --platform linux/amd64,linux/arm64 -t katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
+docker buildx build --platform linux/amd64,linux/arm64 --push -t katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
