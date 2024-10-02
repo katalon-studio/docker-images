07c5627e11fc7651edb1116d8b1bab7cef06b6d5
Huyen Nguyen <huyen.nguyen@katalon.com>
2024-10-02 17:25:10 +0700

    Install more app

--- build/build.sh
+++ build/build.sh
@@ -3,7 +3,6 @@
 set -xe
 
 ksversion=$1
-export DOCKER_BUILDKIT=1
 
 docker buildx create --name mybuilder --driver docker-container --use --bootstrap
-docker build --platform linux/amd64,linux/arm64 -t  katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
+docker buildx build --platform linux/amd64,linux/arm64 -t  katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
