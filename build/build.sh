81ad69e857ddf7d01f0a014b97bcbf7792d09cda
Huyen Nguyen <huyen.nguyen@katalon.com>
2024-10-02 16:41:32 +0700

    Remove unuse code

--- build/build.sh
+++ build/build.sh
@@ -6,5 +6,4 @@ ksversion=$1
 export DOCKER_BUILDKIT=1
 
 docker buildx create --name mybuilder --driver docker-container --use --bootstrap
-docker buildx inspect --bootstrap
-docker build --platform linux/amd64,linux/arm64 -t  katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .dock
+docker build --platform linux/amd64,linux/arm64 -t  katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
