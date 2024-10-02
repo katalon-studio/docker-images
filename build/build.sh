b15d538b78b7cba36c754e8eb846365b00378e30
Huyen Nguyen <huyen.nguyen@katalon.com>
2024-10-03 06:06:33 +0700

    Authorize before buiding

--- build/build.sh
+++ build/build.sh
@@ -7,4 +7,5 @@ export DOCKER_BUILDKIT=1
 
 # docker buildx create --name mybuilder --driver docker-container --use --bootstrap
 docker buildx ls
-docker buildx build --platform linux/amd64,linux/arm64 --load -t  katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
+docker buildx inspect --bootstrap
+docker buildx build --platform linux/amd64,linux/arm64 --push -t  katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
