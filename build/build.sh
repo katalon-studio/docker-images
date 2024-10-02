5d9e38f60c49349ff8cfff13bc1c6d0877257f27
Huyen Nguyen <huyen.nguyen@katalon.com>
2024-10-02 11:29:31 +0700

    Revert "Authorize with AWS before Tag"
    This reverts commit 6c263590c0ffb8ac8c63e792ecf28265133cf84d.

--- build/build.sh
+++ build/build.sh
@@ -7,4 +7,4 @@ export DOCKER_BUILDKIT=1
 
 docker buildx create --name mybuilder --use
 docker buildx inspect --bootstrap
-docker buildx build --platform linux/amd64,linux/arm64 --push -t katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
+docker buildx build -t katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
