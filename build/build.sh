44039cf157e04c24bfa681f3789dc8202f8ce596
Huyen Nguyen <huyen.nguyen@katalon.com>
2024-10-02 16:31:47 +0700

    Fix format to build

--- build/build.sh
+++ build/build.sh
@@ -5,6 +5,6 @@ set -xe
 ksversion=$1
 export DOCKER_BUILDKIT=1
 
-docker buildx create --name mybuilder --use
+docker buildx create --name mybuilder --driver docker-container --use --bootstrap
 docker buildx inspect --bootstrap
-docker build -t katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
+docker build --platform linux/amd64,linux/arm64 -t  katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .dock
