dcc6d92e6f874e7f663621b5bdea45e01689265b
Huyen Nguyen <huyen.nguyen@katalon.com>
2024-10-03 08:07:53 +0700

    push directly while building with tag

--- build/build.sh
+++ build/build.sh
@@ -3,9 +3,10 @@
 set -xe
 
 ksversion=$1
+imageTag=$2
 export DOCKER_BUILDKIT=1
 
 # docker buildx create --name mybuilder --driver docker-container --use --bootstrap
 docker buildx ls
 docker buildx inspect --bootstrap
-docker buildx build --platform linux/amd64,linux/arm64 --load -t  katalon-katalon -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
+docker buildx build --platform linux/amd64,linux/arm64 --push -t  katalon-katalon:$imageTag -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
