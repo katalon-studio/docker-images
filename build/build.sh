ab2312907096350e18cb4334b633904023726636
Huyen Nguyen <huyen.nguyen@katalon.com>
2024-10-03 10:13:59 +0700

    change image name

--- build/build.sh
+++ build/build.sh
@@ -9,4 +9,4 @@ export DOCKER_BUILDKIT=1
 # docker buildx create --name mybuilder --driver docker-container --use --bootstrap
 docker buildx ls
 docker buildx inspect --bootstrap
-docker buildx build --platform linux/amd64,linux/arm64 --push -t  katalon-katalon:$imageTag -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
+docker buildx build --platform linux/amd64,linux/arm64 --push -t  katalonstudio/katalon:$imageTag -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
