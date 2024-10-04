e2fb2131a3ef3ca4468a64bd42a47163c0d74398
Huyen Nguyen <huyen.nguyen@katalon.com>
2024-10-04 15:44:22 +0700

    Update criteria

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -21,9 +21,9 @@ cd $KATALON_KATALON_ROOT_DIR
 echo "Install Katalon"
 katalon_version=$(cut -d '-' -f 1 <<< "$KATALON_STUDIO_VERSION")
 katalon_directory="$version"
-katalon_package="Katalon_Studio_Engine_Linux_arm64-$katalon_version.tar.gz"
-katalon_unzipped_directory="Katalon_Studio_Engine_Linux_arm64-$katalon_version"
-if [ $TARGETPLATFORM -eq "linux/arm64" ]; then
+katalon_package="Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz"
+katalon_unzipped_directory="Katalon_Studio_Engine_Linux_64-$katalon_version"
+if [ "$TARGETPLATFORM" -eq "linux/arm64" ]; then
     katalon_package="Katalon_Studio_Engine_Linux_arm64-$katalon_version.tar.gz"
     katalon_unzipped_directory="Katalon_Studio_Engine_Linux_arm64-$katalon_version"
 fi
