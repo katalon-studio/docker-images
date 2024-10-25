3e707d08aaedbf7a45f629ef71ce9de1e99456d4
Vu Than <158494061+thvu-katalon@users.noreply.github.com>
2024-10-25 21:30:18 +0700

    Fix build issue (#191)

--- build/build.sh
+++ build/build.sh
@@ -6,7 +6,7 @@ ksversion=$1
 firstTagName=$2
 secondTagName=$3
 
-if [-z "$secondTagName"]; then
+if [ -z "$secondTagName" ]; then
     docker buildx build --platform linux/amd64,linux/arm64 --push -t  katalonstudio/katalon:$firstTagName -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
 else
     docker buildx build --platform linux/amd64,linux/arm64 --push -t  katalonstudio/katalon:$firstTagName -t  katalonstudio/katalon:$secondTagName -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion .
