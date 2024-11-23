cd7dc581915a1ed521079b34956705bac6eb5dd0
ThuanKatalon <thuan.nguyen@katalon.com>
2024-11-23 15:54:32 +0700

    1

--- build/build-for-ecr.sh
+++ build/build-for-ecr.sh
@@ -6,4 +6,4 @@ ksversion=$1
 tagName=$2
 isProduction=$3
 
-docker buildx build --platform linux/amd64,linux/arm64 -t  002582244933.dkr.ecr.us-east-1.amazonaws.com/katalon-studio-tmp:$tagName -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion --build-arg IS_PRODUCTION=$isProduction.
+docker buildx build --platform linux/amd64,linux/arm64 -t  002582244933.dkr.ecr.us-east-1.amazonaws.com/katalon-studio-tmp:$tagName -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion --build-arg IS_PRODUCTION=$isProduction .
