61fd5513304b42c251dabbde8c1f19fc88661f30
ThuanKatalon <thuan.nguyen@katalon.com>
2024-11-23 19:05:18 +0700

    1

--- src/scripts/setup-slim.sh
+++ src/scripts/setup-slim.sh
@@ -4,9 +4,9 @@ set -xe
 
 TARGETPLATFORM=$1
 
-# # copy scripts
-# mkdir -p $KATALON_KATALON_ROOT_DIR
-# cd $KATALON_KATALON_ROOT_DIR
+# copy scripts
+mkdir -p $KATALON_KATALON_ROOT_DIR
+cd $KATALON_KATALON_ROOT_DIR
 
 echo "Install Katalon"
 katalon_version=$(cut -d '-' -f 1 <<< "$KATALON_STUDIO_VERSION")
@@ -22,29 +22,27 @@ PACKAGE_DOMAIN="download.staging.katalon.com"
 if [ "$IS_PRODUCTION" == "true" ]; then
     PACKAGE_DOMAIN="download.katalon.com"
 fi
-# # general link
-echo "Result"
-echo $katalon_package https://$PACKAGE_DOMAIN/$KATALON_STUDIO_VERSION/$katalon_package
+# general link
 wget -O $katalon_package https://$PACKAGE_DOMAIN/$KATALON_STUDIO_VERSION/$katalon_package
-# # beta link
-# # wget -O $katalon_package https://download.katalon.com/release-beta/$katalon_version/Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz
-# ls
-# tar -xvzf $katalon_package -C $KATALON_KATALON_INSTALL_DIR_PARENT
-# ls
+# beta link
+# wget -O $katalon_package https://download.katalon.com/release-beta/$katalon_version/Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz
+ls
+tar -xvzf $katalon_package -C $KATALON_KATALON_INSTALL_DIR_PARENT
+ls
 
-# # Remove webdrivermanager.jar
-# rm $KATALON_KATALON_INSTALL_DIR_PARENT/$katalon_unzipped_directory/configuration/resources/tools/webdriver/webdrivermanager-5.8.0-fat.jar
+# Remove webdrivermanager.jar
+rm $KATALON_KATALON_INSTALL_DIR_PARENT/$katalon_unzipped_directory/configuration/resources/tools/webdriver/webdrivermanager-5.8.0-fat.jar
 
-# rm $katalon_package
-# mv $KATALON_KATALON_INSTALL_DIR_PARENT/$katalon_unzipped_directory $KATALON_KATALON_INSTALL_DIR
-# chmod u+x $KATALON_KATALON_INSTALL_DIR/katalonc
-# chmod -R u+x $KATALON_KATALON_INSTALL_DIR/configuration/resources/drivers
-# echo "Katalon Studio $version" >> $KATALON_VERSION_FILE
+rm $katalon_package
+mv $KATALON_KATALON_INSTALL_DIR_PARENT/$katalon_unzipped_directory $KATALON_KATALON_INSTALL_DIR
+chmod u+x $KATALON_KATALON_INSTALL_DIR/katalonc
+chmod -R u+x $KATALON_KATALON_INSTALL_DIR/configuration/resources/drivers
+echo "Katalon Studio $version" >> $KATALON_VERSION_FILE
 
-# chmod -R 777 $KATALON_ROOT_DIR
-# chmod -R 777 $KATALON_KATALON_INSTALL_DIR
+chmod -R 777 $KATALON_ROOT_DIR
+chmod -R 777 $KATALON_KATALON_INSTALL_DIR
 
-# # clean up
-# apt clean all
-# rm -rf /var/lib/apt/lists/*
-# cat $KATALON_VERSION_FILE
+# clean up
+apt clean all
+rm -rf /var/lib/apt/lists/*
+cat $KATALON_VERSION_FILE
