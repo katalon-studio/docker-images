f52de67404d59d00ed0361a39aa31d776e034418
ThuanKatalon <thuan.nguyen@katalon.com>
2024-11-23 19:04:12 +0700

    1

--- build/build.sh
+++ build/build.sh
@@ -8,7 +8,7 @@ isProduction=$3
 secondTagName=$4
 
 if [ -z "$secondTagName" ]; then
-    docker buildx build --platform linux/amd64,linux/arm64 --push -t  katalonstudio/katalon-tmp:$firstTagName -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion --build-arg IS_PRODUCTION=$isProduction .
+    docker buildx build --platform linux/amd64,linux/arm64 --push -t  katalonstudio/katalon:$firstTagName -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion --build-arg IS_PRODUCTION=$isProduction .
 else
-    docker buildx build --platform linux/amd64,linux/arm64 --push -t  katalonstudio/katalon-tmp:$firstTagName -t  katalonstudio/katalon-tmp:$secondTagName -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion --build-arg IS_PRODUCTION=$isProduction .
+    docker buildx build --platform linux/amd64,linux/arm64 --push -t  katalonstudio/katalon:$firstTagName -t  katalonstudio/katalon:$secondTagName -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion --build-arg IS_PRODUCTION=$isProduction .
 fi
