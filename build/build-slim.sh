e3a602424a10b30713f0ba195251c865e89f13ac
ThuanKatalon <95392858+ThuanKatalon@users.noreply.github.com>
2024-11-25 18:04:24 +0700

    CO-3487, CO-3488: Conditionaly download the KRE package from internal or external S3 bucket (#196)

--- build/build-slim.sh
+++ build/build-slim.sh
@@ -4,10 +4,11 @@ set -xe
 
 ksversion=$1
 firstTagName=$2
-secondTagName=$3
+isProduction=$3
+secondTagName=$4
 
 if [ -z "$secondTagName" ]; then
-    docker buildx build --platform linux/amd64,linux/arm64 --push -t  katalonstudio/katalon:$firstTagName -f src/Dockerfile.slim --build-arg KATALON_STUDIO_VERSION=$ksversion .
+    docker buildx build --platform linux/amd64,linux/arm64 --push -t  katalonstudio/katalon:$firstTagName -f src/Dockerfile.slim --build-arg KATALON_STUDIO_VERSION=$ksversion --build-arg IS_PRODUCTION=$isProduction .
 else
-    docker buildx build --platform linux/amd64,linux/arm64 --push -t  katalonstudio/katalon:$firstTagName -t  katalonstudio/katalon:$secondTagName -f src/Dockerfile.slim --build-arg KATALON_STUDIO_VERSION=$ksversion .
+    docker buildx build --platform linux/amd64,linux/arm64 --push -t  katalonstudio/katalon:$firstTagName -t  katalonstudio/katalon:$secondTagName -f src/Dockerfile.slim --build-arg KATALON_STUDIO_VERSION=$ksversion --build-arg IS_PRODUCTION=$isProduction .
 fi
