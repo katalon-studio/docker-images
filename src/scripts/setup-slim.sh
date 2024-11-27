c0b06766d460ebcc420bc0e90fd0f037632e218d
Duy Lam <duy.lam@katalon.com>
2024-11-27 17:01:56 +0700

    Adapt KRE download links uploaded by both Azure DevOps and GitHub Action

--- src/scripts/setup-slim.sh
+++ src/scripts/setup-slim.sh
@@ -17,13 +17,35 @@ if [ "$TARGETPLATFORM" == "linux/arm64" ]; then
     katalon_package="Katalon_Studio_Engine_Linux_arm64-$katalon_version.tar.gz"
     katalon_unzipped_directory="Katalon_Studio_Engine_Linux_arm64-$katalon_version"
 fi
+
 # Switch package domain for downloading
 PACKAGE_DOMAIN="download.staging.katalon.com"
 if [ "$IS_PRODUCTION" == "true" ]; then
     PACKAGE_DOMAIN="download.katalon.com"
 fi
+
+package_download_url=https://$PACKAGE_DOMAIN/$KATALON_STUDIO_VERSION/$katalon_package
+
+#
+# At this moment, both Azure DevOps and GitHub Actions are being used and it's only
+# the GitHub Action to store the build file at the download.staging.katalon.com for
+# non-production builds. Once the Azure DevOps pipeline is sunset, the download link
+# per the above statement is correct. To keep the build working for both pipelines,
+# the following code is added to check the file existing as extra step
+#
+set +e
+wget --spider -q $package_download_url
+if [ $? -ne 0 ]; then
+    if [ "$IS_PRODUCTION" == "true" ]; then
+        package_download_url=https://download.staging.katalon.com/$KATALON_STUDIO_VERSION/$katalon_package
+    else
+        package_download_url=https://download.katalon.com/$KATALON_STUDIO_VERSION/$katalon_package
+    fi
+fi
+set -e
+
 # general link
-wget -O $katalon_package https://$PACKAGE_DOMAIN/$KATALON_STUDIO_VERSION/$katalon_package
+wget -O $katalon_package $package_download_url
 # beta link
 # wget -O $katalon_package https://download.katalon.com/release-beta/$katalon_version/Katalon_Studio_Engine_Linux_64-$katalon_version.tar.gz
 ls
