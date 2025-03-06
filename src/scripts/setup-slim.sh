2786470723ad4a2f4ebd6acffc4825267fb7f97c
an.qnguyen <>
2025-03-06 09:34:58 +0700

    update

--- src/scripts/setup-slim.sh
+++ src/scripts/setup-slim.sh
@@ -7,8 +7,16 @@ TARGETPLATFORM=$1
 cd $KATALON_BASE_ROOT_DIR
 
 echo "Install Mozilla Firefox"
-apt -y install firefox
-snap install firefox
+install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | tee /etc/apt/preferences.d/mozilla
+apt-get update
+apt-get install firefox -y
 # Install 'pulseaudio' package to support WebRTC audio streams
 apt -y install pulseaudio
 echo "$(firefox -version)" >> $KATALON_VERSION_FILE
