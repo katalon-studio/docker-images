fa73406eb5e0e47df41ccd2dd4cd4d582edeb985
Vu Than <vu.than@katalon.com>
2025-05-06 14:26:27 +0700

    Remove ARM image in build commands

--- build/build-for-ecr.sh
+++ build/build-for-ecr.sh
@@ -6,4 +6,4 @@ ksversion=$1
 tagName=$2
 isProduction=$3
 
-docker buildx build --platform linux/amd64,linux/arm64 --push -t  002582244933.dkr.ecr.us-east-1.amazonaws.com/katalon-studio:$tagName -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion --build-arg IS_PRODUCTION=$isProduction .
+docker buildx build --platform linux/amd64 --push -t  002582244933.dkr.ecr.us-east-1.amazonaws.com/katalon-studio:$tagName -f src/Dockerfile --build-arg KATALON_STUDIO_VERSION=$ksversion --build-arg IS_PRODUCTION=$isProduction .
